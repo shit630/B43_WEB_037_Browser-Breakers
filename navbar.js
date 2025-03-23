82fb1e7c0d1f0cef534011e238bf0427dbe8882a
Soumen Shit <soumenshit907@gmail.com>
2025-03-23 21:56:54 +0530

    Day 5 tasks added

--- navbar.js
+++ navbar.js
@@ -1,6 +1,9 @@
+
+
 let loginForm = document.querySelector(".loginForm");
 loginForm.addEventListener("click", goToLoginPage);
 
 function goToLoginPage(){
     window.location.href = "signin.html"
 }
+
